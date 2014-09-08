cccd0fc0bd9bc7b164315c360b95705fca768072
Greg Phillips <gap023@gmail.com>
2014-09-08 17:23:00 -0400

    use amazonica instead of clj-aws-s3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject s3-util "0.1.0-SNAPSHOT"
+(defproject s3-util "0.2.0-SNAPSHOT"
   :description "Some utility functions for working with Amazon S3."
   :url "https://github.com/gphil/s3-util"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-aws-s3 "0.3.7"]
-                 [org.clojure/clojure "1.4.0"]])
+  :dependencies [[amazonica "0.2.24"]
+                 [org.clojure/clojure "1.6.0"]])
--- src/s3_util/core.clj
+++ src/s3_util/core.clj
@@ -1,29 +1,36 @@
 (ns s3-util.core
   (:refer-clojure :exclude [spit])
-  (:require [aws.sdk.s3 :as s3]))
+  (:require [amazonica.aws.s3 :as s3]))
 
 (defn- create-bucket-if-needed
   "Creates a new S3 bucket with name \"bucket\" using AWS credentials
   \"cred\" if it does not already exist."
   [cred bucket]
-  (if (not (s3/bucket-exists? cred bucket))
+  (if (not (s3/does-bucket-exist cred bucket))
     (s3/create-bucket cred bucket)))
 
 (defn spit
   "Puts a new S3 key/value pair into \"bucket\" using AWS credentials
   \"cred\". Creates the bucket automatically if it does not already
-  exist. Passes metadata through to aws.sdk.s3/put-object."
+  exist."
   [cred bucket key value & [metadata]]
   (do
     (create-bucket-if-needed cred bucket)
-    (s3/put-object cred bucket key value metadata)))
+    (s3/put-object cred
+                   :bucket-name bucket
+                   :key key
+                   :input-stream value
+                   :metadata metadata)))
 
 (defn spit-map
   "Puts a new S3 key/value pair into \"bucket\" using AWS credentials
   \"cred\" for each key/value pair in map. Creates the bucket
-  automatically if it does not already exist. Passes metadata through
-  to aws.sdk.s3/put-object."
+  automatically if it does not already exist."
   [cred bucket m & [metadata]]
   (do
     (create-bucket-if-needed cred bucket)
-    (map (fn [[k v]] (s3/put-object cred bucket k v metadata)) m)))
+    (map (fn [[k v]] (s3/put-object cred
+                                    :bucket-name bucket
+                                    :key k
+                                    :input-stream v
+                                    :metadata metadata)) m)))
